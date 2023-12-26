f7a567a7429d6d87148077736c690180af78f8a2
WhiteStar <jobsss.ru>
2023-12-26 17:58:58 +0500

    note application

---
+++ android/app/src/main/kotlin/com/example/newappss/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newappss
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('111'),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// i39100f82150jpeg78100
